0da13bdfdfcf1b49043709db35258b5d0813fce4
Mehmet Çetin <mehmet.cetin@kafein.com.tr>
2022-11-14 02:08:44 +0300

    TASK-6 limit added for allowed characters

--- src/main/java/com/example/wordrelation/model/WordRelation.java
+++ src/main/java/com/example/wordrelation/model/WordRelation.java
@@ -13,6 +13,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Data
@@ -26,9 +27,11 @@ public class WordRelation {
     private Long id;
 
     @NotBlank(message = "First Word must not be empty!")
+    @Pattern(regexp = "^[A-Za-z]*$", message = "Only characters from A to Z(both lower and uppercase) are allowed")
     private String firstWord;
 
     @NotBlank(message = "Second Word must not be empty!")
+    @Pattern(regexp = "^[A-Za-z]*$", message = "Only characters from A to Z(both lower and uppercase) are allowed")
     private String secondWord;
 
     @NotBlank(message = "Relation must not be empty!")
